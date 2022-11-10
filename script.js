d9e1b4764334983f427402d7832978fe8c0af68b
L-V-Dragon <viotti37@gmail.com>
2022-11-10 16:28:05 +0100

    Es finito senza layout HTML

--- script.js
+++ script.js
@@ -2,25 +2,33 @@ let tratta = prompt("Inserisci il la tratta");
 let età = prompt("Inserisci la tua età");
 let tariffa = 0.21;
 let prezzo;
-
+let sconto;
+let totale;
 
 prezzo = tariffa * tratta;
 
 if(età >= 4 && età < 18){
 
-    prezzo / 20 * 100;
+   sconto = prezzo / 100 * 20;
+   totale = prezzo - sconto;
 
 } else if(età > 65){
 
-    prezzo /40 * 100;
+    sconto = prezzo / 100 * 40;
+    totale = prezzo - sconto;
+
+} else {
+
+    totale = prezzo;
 
 }
 
+totale = totale.toFixed(2);
 
 
 console.log(
     `
-        Log: "Il totale è: ${prezzo}&#8364; " 
+        Log: "Il totale è: ${totale}€" 
     `
 );
 
